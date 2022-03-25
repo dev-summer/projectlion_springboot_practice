bb18b8eb15fce23b54948c6f2174b365e839048f
dev-summer <summerly812@gmail.com>
2022-03-25 17:22:56 +0900

    Ch.8-2 permitAll

---
+++ auth/src/test/java/dev/summer/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.summer.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
