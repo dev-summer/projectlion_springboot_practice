7123ee0b6b5d6ee6d442a6df17e67e6056a055b3
dev-summer <summerly812@gmail.com>
2022-03-27 19:41:27 +0900

    Ch.8-3 AuthFacade

--- auth/src/main/java/dev/summer/auth/controller/HomeController.java
+++ auth/src/main/java/dev/summer/auth/controller/HomeController.java
@@ -1,7 +1,9 @@
 package dev.summer.auth.controller;
 
+import dev.summer.auth.infra.AuthenticationFacade;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -14,13 +16,18 @@ import java.security.Principal;
 @RequestMapping("home")
 public class HomeController {
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    private final AuthenticationFacade authFacade;
+
+    public HomeController(@Autowired AuthenticationFacade authFacade) {
+        this.authFacade = authFacade;
+    }
 
     @GetMapping
     public String home(){
         try {
 //            logger.info("connected user: {}", principal.getName());
 //            logger.info("connected user: {}",authentication.getName());
-            logger.info("connected user: {}", SecurityContextHolder.getContext().getAuthentication().getName());
+            logger.info("connected user: {}", authFacade.getUserName());
         } catch (NullPointerException e) {
             logger.info("no user logged in");
         }
