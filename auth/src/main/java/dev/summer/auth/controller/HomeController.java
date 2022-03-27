e75e3c7ae8f396eceefd310375a0c2f2599c25ef
dev-summer <summerly812@gmail.com>
2022-03-27 19:33:32 +0900

    Ch.8-3 SecurityContextHolder

--- auth/src/main/java/dev/summer/auth/controller/HomeController.java
+++ auth/src/main/java/dev/summer/auth/controller/HomeController.java
@@ -3,6 +3,7 @@ package dev.summer.auth.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +16,11 @@ public class HomeController {
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping
-    public String home(Authentication authentication){
+    public String home(){
         try {
 //            logger.info("connected user: {}", principal.getName());
-            logger.info("connected user: {}",authentication.getName());
+//            logger.info("connected user: {}",authentication.getName());
+            logger.info("connected user: {}", SecurityContextHolder.getContext().getAuthentication().getName());
         } catch (NullPointerException e) {
             logger.info("no user logged in");
         }
