5f5a0f04cd1dcbae05d12c0e65e7eedc356743bb
dev-summer <summerly812@gmail.com>
2022-03-27 19:30:00 +0900

    Ch.8-3 HomeController authentication

--- auth/src/main/java/dev/summer/auth/controller/HomeController.java
+++ auth/src/main/java/dev/summer/auth/controller/HomeController.java
@@ -2,9 +2,12 @@ package dev.summer.auth.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("home")
@@ -12,7 +15,13 @@ public class HomeController {
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
     @GetMapping
-    public String home(){
+    public String home(Authentication authentication){
+        try {
+//            logger.info("connected user: {}", principal.getName());
+            logger.info("connected user: {}",authentication.getName());
+        } catch (NullPointerException e) {
+            logger.info("no user logged in");
+        }
         return "index";
     }
 }
