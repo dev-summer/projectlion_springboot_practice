8b4575fd49e48e6353c9c4261ef5302bbf0036da
dev-summer <summerly812@gmail.com>
2022-04-01 16:14:06 +0900

    Ch.9-2 publisher-subscriber queue

--- publisher/src/main/java/dev/summer/publisher/config/PublisherConfig.java
+++ publisher/src/main/java/dev/summer/publisher/config/PublisherConfig.java
@@ -1,4 +1,17 @@
 package dev.summer.publisher.config;
 
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class PublisherConfig {
+
+    @Bean
+    public FanoutExchange fanoutExchange(){
+        return new FanoutExchange("boot.fanout.exchange");
+    }
+    // FanoutExchange: 구독되어있는 모든 큐에 메시지를 전달
+
+
 }
---
+++ subscriber/src/main/java/dev/summer/subscriber/config/SubscriberConfig.java
@@ -0,0 +1,30 @@
+package dev.summer.subscriber.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SubscriberConfig {
+
+    @Bean
+    public FanoutExchange fanoutExchange(){
+        return new FanoutExchange("boot.fanout.exchange");
+    }
+
+    @Bean
+    public Queue autoGenQueue(){
+        return new AnonymousQueue();
+    }
+
+    @Bean
+    public Binding binding(
+            Queue queue,
+            FanoutExchange fanoutExchange
+    ){
+        return BindingBuilder
+                .bind(queue)
+                .to(fanoutExchange);
+    }
+    // queue를 fanoutexchange에 연결(bind)
+}
