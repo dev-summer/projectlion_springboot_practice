89ca79f04281948d85cc5b5b3e8da59d6dc4a7b5
dev-summer <summerly812@gmail.com>
2022-04-05 15:09:10 +0900

    Ch.10-3 Spring Cloud Gateway based on JAVA sourcecode

---
+++ gateway/src/main/java/dev/summer/gateway/RoutingConfig.java
@@ -0,0 +1,23 @@
+package dev.summer.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RoutingConfig {
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
+        return builder.routes()
+                .route("community-shop", predicate -> predicate
+                            .path("/api/shop/**") // Gateway가 받는 경로 기준으로 작성
+                            .filters(filter -> filter
+                                    .rewritePath(
+                                            "/api/(?<path>.*)",
+                                            "/${path}"
+                                    ))
+                            .uri("http://localhost:8081"))
+                .build();
+    }
+}
