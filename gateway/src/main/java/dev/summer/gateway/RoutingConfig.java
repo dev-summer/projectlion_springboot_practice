102f6e8d48f0e083633dece482757f993a784a63
dev-summer <summerly812@gmail.com>
2022-04-05 15:13:17 +0900

    Ch.10-3 Spring Cloud Gateway based on Application File

--- gateway/src/main/java/dev/summer/gateway/RoutingConfig.java
+++ gateway/src/main/java/dev/summer/gateway/RoutingConfig.java
@@ -5,9 +5,9 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class RoutingConfig {
-    @Bean
+//    @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route("community-shop", predicate -> predicate
